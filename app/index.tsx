72c1defbece000c5877147059d2bd0f31a72cd7a
petruswagnavian <peterwang51924@gmail.com>
2025-07-31 15:33:36 -0700

    Smoother loading and removed inertia

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-light-200 font-bold">John MacArthur</Text>
+      <Text className="text-5xl text-light-200 font-bold">R.C. Sproul</Text>
         <Link href="/mapper">Mapper</Link>
     </View>
   );
