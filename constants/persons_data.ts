0cc127525cb656987e42cdeb86934d5f318786c4
petruswagnavian <peterwang51924@gmail.com>
2025-08-04 23:52:23 -0700

    Added a few more people

--- constants/persons_data.ts
+++ constants/persons_data.ts
@@ -9,12 +9,36 @@ export type Person = {
 
 export const all_persons: Person[] = [
     {
+        pid: "matthew_the_apostle",
+        lat: 32.88,
+        lon: 35.57,
+        imageUrl: 'https://i0.wp.com/www.thebestcatholic.com/wp-content/uploads/2016/09/The-Best-Catholic.Matthew5a319b0c116995afb26cab5af9e51e4c.wix_mp_1024.jpeg',
+        fromYear: 4,
+        toYear: 68
+    },
+    {
         pid: "paul_of_tarsus",
         lat: 36.92,
         lon: 34.90,
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Giovanni_Francesco_Barbieri_-_Saint_Paul.jpg/960px-Giovanni_Francesco_Barbieri_-_Saint_Paul.jpg?20190309231900',
         fromYear: 5,
         toYear: 65
+    },
+    {
+        pid: "clement_of_rome",
+        lat: 41.88,
+        lon: 12.59,
+        imageUrl: 'https://lonelypilgrim.com/wp-content/uploads/2014/05/clement-of-rome.jpg?w=214',
+        fromYear: 35,
+        toYear: 101
+    },
+    {
+        pid: "papias_of_hierapolis",
+        lat: 37.91,
+        lon: 29.11,
+        imageUrl: 'https://pbcdn1.podbean.com/imglogo/ep-logo/pbblog17411067/papias.jpeg',
+        fromYear: 60,
+        toYear: 130
     },
     {
         pid: "augustine_of_hippo",
