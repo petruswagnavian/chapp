1e91d471a6d00a4c2f0c8d742d9b29e6d66a9610
petruswagnavian <peterwang51924@gmail.com>
2025-09-16 22:17:51 -0700

    Added more people and info

--- constants/persons_data.ts
+++ constants/persons_data.ts
@@ -1,6 +1,8 @@
 export type Person = {
     pid: string;
     displayName: string;
+    birthplace?: string;
+    deathplace?: string;
     lat: number;
     lon: number;
     imageUrl: string;
@@ -16,6 +18,8 @@ export const all_persons: Person[] = [
     {
         pid: "peter_the_apostle",
         displayName: "Peter the Apostle",
+        birthplace: "Bethsaida, Galilee, Judaea, Roman Empire",
+        deathplace: "Rome, Italia, Roman Empire",
         lat: 32.91089391890956,
         lon: 35.63200492242213,
         imageUrl: 'https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Saint_Peter-Sinai_%286th_Century%29_Crop.jpg/1024px-Saint_Peter-Sinai_%286th_Century%29_Crop.jpg',
@@ -28,6 +32,8 @@ export const all_persons: Person[] = [
     {
         pid: "matthew_the_apostle",
         displayName: "Matthew the Apostle",
+        birthplace: "Capernaum, Galilee, Judaea, Roman Empire",
+        deathplace: "Ethiopia or Persia",
         lat: 32.88,
         lon: 35.57,
         imageUrl: 'https://i0.wp.com/www.thebestcatholic.com/wp-content/uploads/2016/09/The-Best-Catholic.Matthew5a319b0c116995afb26cab5af9e51e4c.wix_mp_1024.jpeg',
@@ -38,8 +44,24 @@ export const all_persons: Person[] = [
         camps: ["Apostle"]
     },
     {
+        pid: "james_the_just",
+        displayName: "James the Just",
+        birthplace: "Nazareth, Galilee, Judaea, Roman Empire",
+        deathplace: "Jerusalem, Judaea, Roman Empire",
+        lat: 32.70234804256465,
+        lon: 35.297873906995605,
+        imageUrl: 'https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/9/96/Saint_James_the_Just.jpg',
+        fromYear: 4,
+        fromApprox: true,
+        toYear: 62,
+        toApprox: true,
+        camps: ["Apostle"]
+    },
+    {
         pid: "paul_of_tarsus",
         displayName: "Paul of Tarsus",
+        birthplace: "Tarsus, Cilicia, Roman Empire",
+        deathplace: "Rome, Italia, Roman Empire",
         lat: 36.92,
         lon: 34.90,
         imageUrl: 'https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/7/79/Giovanni_Francesco_Barbieri_-_Saint_Paul.jpg/960px-Giovanni_Francesco_Barbieri_-_Saint_Paul.jpg?20190309231900',
@@ -50,8 +72,52 @@ export const all_persons: Person[] = [
         camps: ["Apostle"]
     },
     {
+        pid: "john_the_apostle",
+        displayName: "John the Apostle",
+        birthplace: "Bethsaida, Galilee, Judaea, Roman Empire",
+        deathplace: "Ephesus, Ionia, Asia Minor, Roman Empire",
+        lat: 32.91089391890956,
+        lon: 35.63200492242213,
+        imageUrl: 'https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/7/79/Rubens_apostel_johannes_grt.jpg/1024px-Rubens_apostel_johannes_grt.jpg',
+        fromYear: 6,
+        fromApprox: true,
+        toYear: 100,
+        toApprox: true,
+        camps: ["Apostle"]
+    },
+    {
+        pid: "john_mark_the_evangelist",
+        displayName: "John Mark the Evangelist",
+        birthplace: "Cyrene, Cyrenaica, Roman Empire",
+        deathplace: "Alexandria, Egypt, Roman Empire",
+        lat: 32.819808224773865,
+        lon: 21.859442918983255,
+        imageUrl: 'https://images.weserv.nl/?url=upload.wikimedia.org/wikipedia/commons/thumb/9/92/Frans_Hals_085.jpg/1024px-Frans_Hals_085.jpg',
+        fromYear: 7,
+        fromApprox: true,
+        toYear: 68,
+        toApprox: true,
+        camps: ["Apostle"]
+    },
+    {
+        pid: "luke_the_evangelist",
+        displayName: "Luke the Evangelist",
+        birthplace: "Antioch, Syria, Roman Empire",
+        deathplace: "Thebes, Boeotia, Achaia, Roman Empire",
+        lat: 36.20405931892094,
+        lon: 36.16216594217945,
+        imageUrl: 'https://images.weserv.nl/?url=myocn.net/wp-content/uploads/2019/10/St-Luke-2.jpeg',
+        fromYear: 10,
+        fromApprox: true,
+        toYear: 94,
+        toApprox: true,
+        camps: ["Apostle"]
+    },
+    {
         pid: "clement_of_rome",
         displayName: "Clement of Rome",
+        birthplace: "Rome, Italia, Roman Empire",
+        deathplace: "Chersonesus, Crimea, Bosporan Kingdom, Roman Empire",
         lat: 41.88,
         lon: 12.59,
         imageUrl: 'https://lonelypilgrim.com/wp-content/uploads/2014/05/clement-of-rome.jpg?w=214',
